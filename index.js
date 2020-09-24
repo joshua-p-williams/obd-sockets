c5b9220b73026409429fa9ef784496bf306a3557
Joshua Williams <joshua.p.williams@gmail.com>
2020-09-23 23:13:57 -0500

    Subscribe to OBD events (testing with RPM)

--- index.js
+++ index.js
@@ -8,130 +8,77 @@ var fs = require('fs');
 var creds = '';
 
 var redis = require('redis');
-var client = '';
+var subscriber = '';
 
 // Read credentials from JSON
 fs.readFile('creds.json', 'utf-8', function (err, data) {
-    if (err) throw err;
-    creds = JSON.parse(data);
-    if (creds.user) {
-      client = redis.createClient('redis://' + creds.user + ':' + creds.password + '@' + creds.host + ':' + creds.port);
-    }
-    else {
-      client = redis.createClient('redis://' + creds.host + ':' + creds.port);
-    }
+  if (err) throw err;
+  creds = JSON.parse(data);
+  if (creds.user) {
+    subscriber = redis.createClient('redis://' + creds.user + ':' + creds.password + '@' + creds.host + ':' + creds.port);
+  }
+  else {
+    subscriber = redis.createClient('redis://' + creds.host + ':' + creds.port);
+  }
 
-    // Redis Client Ready
-    client.once('ready', function () {
+  subscriber.once('ready', function () {
+    console.log('Redis subscriber ready...');
 
-        // Flush Redis DB
-        // client.flushdb();
+    subscriber.on("message", function(channel, message) {
+      //console.log(channel);
+      //console.log(message);
 
-        // Initialize Chatters
-        client.get('chat_users', function (err, reply) {
-            if (reply) {
-                chatters = JSON.parse(reply);
-            }
-        });
-
-        // Initialize Messages
-        client.get('chat_app_messages', function (err, reply) {
-            if (reply) {
-                chat_messages = JSON.parse(reply);
-            }
-        });
+      var data = JSON.parse(message);
+      io.emit(channel, data);
     });
+
+    subscriber.subscribe("RPM");
+  });
+
+  subscriber.on("error", function (error) {
+    console.error(error);
+  });
 });
 
 var port = process.env.PORT || 8080;
 
 // Start the Server
 http.listen(port, function () {
-    console.log('Server Started. Listening on *:' + port);
+  console.log('Server Started. Listening on *:' + port);
 });
-
-// Store people in chatroom
-var chatters = [];
-
-// Store messages in chatroom
-var chat_messages = [];
 
 // Express Middleware
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 
 // Render Main HTML file
 app.get('/', function (req, res) {
-    res.sendFile('views/index.html', {
-        root: __dirname
-    });
-});
-
-// API - Join Chat
-app.post('/join', function (req, res) {
-    var username = req.body.username;
-    if (chatters.indexOf(username) === -1) {
-        chatters.push(username);
-        client.set('chat_users', JSON.stringify(chatters));
-        res.send({
-            'chatters': chatters,
-            'status': 'OK'
-        });
-    } else {
-        res.send({
-            'status': 'FAILED'
-        });
-    }
-});
-
-// API - Leave Chat
-app.post('/leave', function (req, res) {
-    var username = req.body.username;
-    chatters.splice(chatters.indexOf(username), 1);
-    client.set('chat_users', JSON.stringify(chatters));
-    res.send({
-        'status': 'OK'
-    });
+  res.sendFile('views/index.html', {
+    root: __dirname
+  });
 });
 
 // API - Send + Store Message
 app.post('/send_message', function (req, res) {
-    var username = req.body.username;
-    var message = req.body.message;
-    chat_messages.push({
-        'sender': username,
-        'message': message
-    });
-    client.set('chat_app_messages', JSON.stringify(chat_messages));
-    res.send({
-        'status': 'OK'
-    });
+  var message = req.body.message;
+  console.log(message);
+  res.send({
+    'status': 'OK'
+  });
 });
 
-// API - Get Messages
-app.get('/get_messages', function (req, res) {
-    res.send(chat_messages);
-});
-
-// API - Get Chatters
-app.get('/get_chatters', function (req, res) {
-    res.send(chatters);
-});
 
 // Socket Connection
 // UI Stuff
 io.on('connection', function (socket) {
 
-    // Fire 'send' event for updating Message list in UI
-    socket.on('message', function (data) {
-        io.emit('send', data);
-    });
+  console.log('Connection to socket established');
 
-    // Fire 'count_chatters' for updating Chatter Count in UI
-    socket.on('update_chatter_count', function (data) {
-        io.emit('count_chatters', data);
-    });
-
+  // Fire 'send' event for updating Message list in UI
+  socket.on('message', function (data) {
+    console.log('Received a socket message');
+    console.log(data);
+  });
 });
